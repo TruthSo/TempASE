381cd118fd4d3dcc6a7840a1afa4124f2b4fd04b
= <wenjun.tan@anacle.com>
2022-02-12 01:30:43 +0800

    .

--- Assets/Scripts/Controller/TestDb.cs
+++ Assets/Scripts/Controller/TestDb.cs
@@ -8,16 +8,23 @@ public class TestDb : MonoBehaviour
     void Start()
     {
         DatabaseController db = new DatabaseController();
+
+        #region Patient Test Data
         db.SelectAllPatients();
         db.SelectPatientById(2);
 
         Patient patient_a = new Patient("Jack");
         db.AddPatient(patient_a);
 
-        Patient existing_patient = new Patient("4","Bollo");
+        Patient existing_patient = new Patient(4,"Bollo");
         db.UpdatePatient(existing_patient);
 
         db.DeletePatient(5);
+        #endregion
+
+        #region Score Test Data
+
+        #endregion
     }
 
     // Update is called once per frame
